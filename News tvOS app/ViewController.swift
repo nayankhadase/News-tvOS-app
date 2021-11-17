798e0bcf9f8210b39efc4a0509e5db85b750f5bd
nayan.khadase <nayan.khadase@mac-nayank>
2021-11-17 16:42:57 +0530

    Initial Commit

---
+++ News tvOS app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News tvOS app
+//
+//  Created by nayan.khadase on 17/11/21.
+//  Copyright © 2021 nayan.khadase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
